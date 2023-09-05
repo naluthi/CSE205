7b487b321bc550c007e76d331627946978e939ae
Nicolas Luthi <116135231+naluthi@users.noreply.github.com>
2023-09-05 03:01:16 +0000

    bubble_sort

--- bubble_sort/main.cpp
+++ bubble_sort/main.cpp
@@ -1,5 +1,50 @@
-// C++ program for bubble sort
+// C++ Program for Bubble Sort
 
 #include <bits/stdc++.h>
 using namespace std;
+
+// Bubble Sort
+int *bubbleSort(int arr[], int number_of_elements) 
+{
+    for (int i = 0; i < number_of_elements - i; i++) 
+    {
+        for (int j = 0; j < number_of_elements - i - 1; j++)
+        if (arr[j] > arr[j + 1]) {
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+    return arr;
+}
+
+
+
+// Function to print array. Array is called by reference
+void print_array(int arr[], int number_of_elements) 
+{
+    for (int i = 0; i < number_of_elements; i++) 
+        cout << arr[i] << " ";
+
+    cout << endl;
+}
+
+
+
+int main() 
+{
+    // Input for # of elements in the array
+    int number_of_elements;
+    cin >> number_of_elements;
+
+    // Input for elements of the array to be sorted
+    int arr [number_of_elements];
+    for (int i = 0; i < number_of_elements; i++)
+        cin >> arr[i];
+
+    int *ans = bubbleSort(arr, number_of_elements);
+    print_array(ans, number_of_elements);
+
+    return 0;
+}
 
