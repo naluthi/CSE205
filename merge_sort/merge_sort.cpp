7b5bc43eac3d8ab75cc89b6ee0d2c9f49d1b1e9c
Nicolas Luthi <116135231+naluthi@users.noreply.github.com>
2023-10-01 02:14:48 +0000

    working merge_sort

--- merge_sort/merge_sort.cpp
+++ merge_sort/merge_sort.cpp
@@ -7,34 +7,63 @@ using namespace std;
 void merge_two_sorted_arrays(long long int array[], int const left, int const mid,
 int const right)
 {
+    long long int i;
+    long long int j;
+
     // Sub-array sizes
-    int const sub_array_one =
-    int const sub_array_two =
+    int const sub_array_one = mid - left + 1;
+    int const sub_array_two = right - mid;
 
     // Create temp arrays
-    long long int *left_array =
-    long long int *right_array =
+    long long int *left_array = new long long int[sub_array_one];
+    long long int *right_array = new long long int[sub_array_two];
 
     /* Copy data to temp arrays leftArray[] and rightArray[] */
-    // Your code here
+    for (i = 0; i < sub_array_one; i++)
+        left_array[i] = array[left + i]; 
+ 
+    for (j = 0; j < sub_array_two; j++)
+        right_array[j] = array[mid + 1  + j]; 
 
 
     /* Indices of the left, right and merged array*/
-    // Your code here
+    long long int index_array_one = 0, index_array_two = 0;
+    int index_merged_array = left;
 
 
     /* Merge the temp arrays back into array[left..right] */
-    // Your code here
+    while (index_array_one < sub_array_one && index_array_two < sub_array_two) 
+    {
+        if (left_array[index_array_one] <= right_array[index_array_two]) 
+        {
+            array[index_merged_array] = left_array[index_array_one];
+            index_array_one++;
+        }
+        else 
+        {
+            array[index_merged_array] = right_array[index_array_two];
+            index_array_two++;
+        }
+        index_merged_array++;
+    }
 
 
-    /* Copy the remaining elements of
-    left[], if there are any */
-    // Your code here
+    /* Copy the remaining elements of left[], if there are any */
+    while (index_array_one < sub_array_one) 
+    {
+        array[index_merged_array] = left_array[index_array_one];
+        index_array_one++;
+        index_merged_array++;
+    }
 
 
-    /* Copy the remaining elements of
-    right[], if there are any */
-    // Your code here
+    /* Copy the remaining elements of right[], if there are any */
+    while (index_array_two < sub_array_two) 
+    {
+        array[index_merged_array] = right_array[index_array_two];
+        index_array_two++;
+        index_merged_array++;
+    }
 
 
     // Delete left and right sub-arrays
@@ -45,6 +74,12 @@ int const right)
 void merge_sort(long long int array[], int const begin, int const end)
 {
     /* Recursive calls of merge_sort and merge_two_sorted_arrays */
-        // Your code here
+    if (begin >= end)
+        return;
+ 
+    int mid = begin + (end - begin) / 2;
+    merge_sort(array, begin, mid);
+    merge_sort(array, mid + 1, end);
+    merge_two_sorted_arrays(array, begin, mid, end);
 }
 
